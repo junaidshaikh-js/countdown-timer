98b6098141e8ffa485cbc4d41bea8675d5ea00c9
Junaid <sj.shaikhjunaid@gmail.com>
2021-10-01 18:23:34 +0530

    add app functionality

--- js/app.js
+++ js/app.js
@@ -22,3 +22,52 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveAway = document.querySelector(".giveaway");
+const deadlineFormat = document.querySelectorAll(".deadline-format h4");
+
+// setup giveaway date
+const futureDate = new Date();
+
+futureDate.setDate(futureDate.getDate() + 10);
+futureDate.setHours(11, 30);
+
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+const month = months[futureDate.getMonth()];
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const minute = futureDate.getMinutes();
+
+giveAway.innerHTML = `Giveaway ends ${weekday}, ${date} ${month} ${year}, ${hour}:${minute}`;
+
+// setup count down
+function calculateRemainingTime() {
+  const currentTime = new Date().getTime();
+  const remainingTime = futureDate.getTime() - currentTime;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  const days = Math.floor(remainingTime / oneDay);
+  const hours = Math.floor((remainingTime % oneDay) / oneHour);
+  const minutes = Math.floor((remainingTime % oneHour) / oneMinute);
+  const seconds = Math.floor((remainingTime % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  function getCorrectFormat(item) {
+    if (item < 10) {
+      return `0${item}`;
+    }
+
+    return item;
+  }
+
+  deadlineFormat.forEach((item, index) => {
+    item.innerHTML = getCorrectFormat(values[index]);
+  });
+}
+
+const intervalId = setInterval(calculateRemainingTime, 1000);
